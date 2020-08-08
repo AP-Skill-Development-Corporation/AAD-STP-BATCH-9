68aec3d0c19d14e4463a296d215c424d40a8ce94
chaitanya <chaitanyakarri963@gmail.com>
2020-08-08 11:44:07 +0530

    08-08-2020

---
+++ ExampleMenu/app/src/main/java/com/example/cherry/examplemenu/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.cherry.examplemenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.text:
+                tv.setText("This is my Textview");
+                break;
+            case R.id.exit:
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                builder.setTitle("AlertDialog");
+                builder.setMessage("Are you sure want to exit ?");
+                builder.setIcon(R.drawable.ic_exit);
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

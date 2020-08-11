5ae830727037ffe43aee93de7be1eec78ac2a375
chaitanya <chaitanyakarri963@gmail.com>
2020-08-11 11:54:57 +0530

    11-08-2020

---
+++ ExampleNav/app/src/main/java/com/example/cherry/examplenav/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.example.cherry.examplenav;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HomeFragment extends Fragment {
+        Button red,green,blue;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        red = v.findViewById(R.id.red);
+        green = v.findViewById(R.id.green);
+        blue = v.findViewById(R.id.blue);
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_redFragment);
+            }
+        });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_greenFragment);
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_blueFragment);
+            }
+        });
+        return v;
+    }
+}
